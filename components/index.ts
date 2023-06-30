683342ad3cf66220c0fbd399eae417eee6cb84a9
Michael Schlueter <schluet0r4life@gmail.com>
2023-06-30 14:00:53 +0200

    add car catalogue category to main page

--- components/index.ts
+++ components/index.ts
@@ -2,5 +2,7 @@ import CustomButton from "./CustomButton";
 import Hero from "./Hero";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
+import SearchBar from "./SearchBar";
+import CustomFilter from "./CustomFilter";
 
-export { CustomButton, Hero, Navbar, Footer };
+export { CustomButton, Hero, Navbar, Footer, SearchBar, CustomFilter };
