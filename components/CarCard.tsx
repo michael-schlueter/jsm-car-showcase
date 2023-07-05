db2568f5de6c942c63fc3ff48b3d8e9863d3c386
Michael Schlueter <schluet0r4life@gmail.com>
2023-07-05 12:24:38 +0200

    add specific car images

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 
 import CustomButton from "./CustomButton";
 import { CarProps } from "@/types";
-import { calculateCarRent } from "@/utils";
+import { calculateCarRent, generateCarImageUrl } from "@/utils";
 import CarDetails from "./CarDetails";
 
 interface CarCardProps {
@@ -35,7 +35,7 @@ const CarCard = ({ car }: CarCardProps) => {
       </p>
       <div className="relative w-full h-40 my-3 object-contain">
         <Image
-          src="/hero.png"
+          src={generateCarImageUrl(car)}
           alt="car model"
           fill
           priority
